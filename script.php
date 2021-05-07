0b6b5ebfcd9a1bef0325f19f53bbbfa13b243ec3
Greice Mara <sgreicemaraj@gmail.com>
2021-05-07 19:06:31 -0300

    mensagens de erro na validação de resultados

--- script.php
+++ script.php
@@ -1,23 +1,56 @@
 <?php
 
 $classes = [];
-$classes[] = 'infantil';
-$classes[] = 'adolescente';
-$classes[] = 'adulto';
-$classes[] = 'idoso';
-$classes[] = 'nao-compete';
+$classes[] = 'child';
+$classes[] = 'teen';
+$classes[] = 'adult';
+$classes[] = 'senior';
+$classes[] = 'not-applicable';
 
 //print_r($classes);
 
 $name = $_POST['name'];
 $age = $_POST['age'];
 
+//empty() verifica se o campo se resposta está vazio
+if(empty($name) || empty($age)){
+    echo 'Nenhum campo pode estar vazio.';
+    return;
+}
+
+//strlen conta o número de caracteres em uma string
+if(strlen($name) <3 ){
+    echo 'O nome deve conter mais de 3 caracteres.';
+    return;
+}
+
+if(strlen($name) >40 ){
+    echo 'O nome é muito extenso';
+    return;
+}
+
+if(strlen($age) >2){
+    echo 'Digite a idade corretamente.';
+    return;
+}
+
+//is_numeric serve para identificar números
+if(is_numeric($name)){
+    echo 'O nome não pode conter números';
+    return;
+}
+
+if(!is_numeric($age)){
+    echo 'A idade não poder letras.';
+    return;
+}
+
 /*var_dump($name); o var_dump exibe infomações das variáveis
 var_dump($age);*/
 
 if($age >= 6 && $age <= 12){
     for ($i = 0; $i <= count($classes); $i++){
-        if($classes[$i] == 'infantil'){
+        if($classes[$i] == 'child'){
             echo 'O(a) nadador(a) ', $name, ' compete na categoria infantil';
         }
     }
@@ -25,31 +58,32 @@ if($age >= 6 && $age <= 12){
 
 else if($age >= 13 && $age <= 18){
     for ($i = 0; $i <= count($classes); $i++){
-        if($classes[$i] == 'adolescente'){
+        if($classes[$i] == 'tee'){
             echo 'O(a) nadador(a) ', $name, ' compete na categoria adolescente';
-        }
-    }
-}
-
-else if($age >= 60){
-    for ($i = 0; $i <= count($classes); $i++){
-        if($classes[$i] == 'idoso'){
-            echo 'O(a) nadador(a) ', $name, ' compete na categoria idoso';
         }
     }
 }
 
 else if($age >=19 && $age <= 59){
     for ($i = 0; $i <= count($classes); $i++){
-        if($classes[$i] == 'adulto'){
+        if($classes[$i] == 'adult'){
             echo 'O(a) nadador(a) ', $name, ' compete na categoria adulto';
         }
     }
 }
 
+else if($age >= 60){
+    for ($i = 0; $i <= count($classes); $i++){
+        if($classes[$i] == 'senior'){
+            echo 'O(a) nadador(a) ', $name, ' compete na categoria idoso';
+        }
+    }
+}
+
+
 else{
     for ($i = 0; $i <= count($classes); $i++){
-        if($classes[$i] == 'nao-compete'){
+        if($classes[$i] == 'not-applicable'){
             echo 'O(a) nadador(a) ', $name, ' não tem idade para competir';
         }
     }
